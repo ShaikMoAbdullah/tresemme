a5da87748c521c824440925cad07ed698d37b7df
Shaik Mohammad Abdullah <abdullah.shaik@zopsmart.com>
2023-07-01 13:54:52 +0530

    style: improve mobile view of the home page

--- src/Components/MainSceen.js
+++ src/Components/MainSceen.js
@@ -10,6 +10,20 @@ const MainSceen = () => {
       <VectorImage className="vectorImage" />
       <img src={homeImage} alt="Home" className="homeImage" />
       <img src={profileImage} alt="Overlay" className="profileImage" />
+      <div className="description-wrapper">
+        <span className="description">
+          Hi I’m Tia, your stylist, it’s great to meet you I’m here to help you
+          explore TRESemmé’s Hair Pro, and to connect you to style advice from
+          our experts
+        </span>
+      </div>
+      <button className="step-inside-button">
+        <span className="step-inside">STEP INSIDE</span>
+      </button>
+      <div className="terms">
+        By clicking on “Step Inside”, I consent that I have read and understand
+        the <span className="privacy-policy">Privacy Policies</span>
+      </div>
     </div>
   );
 };
